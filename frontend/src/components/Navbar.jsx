e1f3608b88284f878aad23b363fb8a675b2c3d61
emonShimoul <emonshimoul@gmail.com>
2025-10-02 18:42:00 +0600

    completed the navbar and set a toggle button for light and dark mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react'
+import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+    const {colorMode, toggleColorMode} = useColorMode();
   return (
     <Container maxW={"1140px"} px={4}>
         <Flex
@@ -31,7 +34,9 @@ const Navbar = () => {
                     <PlusSquareIcon fontSize={20}/>
                 </Button>
                 </Link>
-                
+                <Button onClick={toggleColorMode}>
+                    {colorMode === "light" ? <IoMoon/> : <LuSun size="20"/>}
+                </Button>
             </HStack>
         </Flex>
     </Container>
