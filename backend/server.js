cc94b840a9d4e3657573a0eebb4e46fc816ff0c6
emonShimoul <emonshimoul@gmail.com>
2025-10-06 13:16:37 +0600

    some setting for run the frontend and backend in a single localhost:5000

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path";
 import {connectDB} from './config/db.js';
 
 import productRoutes from "./routes/product.route.js";
@@ -11,9 +12,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("/*all", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, ()=>{
